609c0483d5a54689446933527d1a12f19c1763ec
Leam Delaney <delaney.leam@heb.com>
2022-08-16 09:54:32 -0500

    initial commit

---
+++ src/test/java/com/candidates/interviewapi/InterviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.candidates.interviewapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
